b002c792f588d30655c4da6570759e287d3b7b4e
jarvis <yunsangr@github.com>
2020-03-09 17:31:06 +0900

    adding hello, bye

--- test.py
+++ test.py
@@ -4,15 +4,14 @@ PATH_OF_GIT_REPO = os.getcwd()  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'comment from python script 1'
 
 
-
-def git_push():
+def git_push(files_to_add, commit_message):
     try:
         repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add("*")
-        repo.index.commit(COMMIT_MESSAGE)
+        repo.git.add(files_to_add)
+        repo.index.commit(commit_message)
         origin = repo.remote(name='origin')
         origin.push()
     except:
         print('Some error occured while pushing the code')    
 
-git_push()
+git_push('*', 'adding hello, bye')
